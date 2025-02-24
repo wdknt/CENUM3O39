a6ed809fef921b91631b26a0030c128c70379b69
wdknt <44008511+wdknt@users.noreply.github.com>
2025-02-24 15:41:24 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/cenum3o39/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xb1, 0xae, 0x9c, 0xa8, 0x7e, 0xf1, 0x2f, 0xd5}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
